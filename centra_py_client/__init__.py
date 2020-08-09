98c0a738f173392d0cbadeda3e3d30b4e29844e2
Yonatan Golan <yonatang93@gmail.com>
2020-08-09 17:07:43 +0300

    Filled setup.py for package building

--- centra_py_client/__init__.py
+++ centra_py_client/__init__.py
@@ -5,4 +5,10 @@ __email__ = 'yonatan.golan@guardicore.com'
 __version__ = '0.1.0'
 
 import logging
+
+from .centra_py_client import Client as Client
+from .centra_py_client import CentraSession as CentraSession
+
+
 logging.getLogger(__name__).addHandler(logging.NullHandler())
+
--- setup.py
+++ setup.py
@@ -7,10 +7,7 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
-requirements = [ ]
+requirements = ['requests', ]  # todo read from requirements.txt
 
 setup_requirements = ['pytest-runner', ]
 
@@ -19,22 +16,20 @@ test_requirements = ['pytest>=3', ]
 setup(
     author="Yonatan Golan",
     author_email='yonatan.golan@guardicore.com',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
     description="Python client for Centra API access.",
     install_requires=requirements,
     license="GNU General Public License v3",
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     include_package_data=True,
     keywords='centra_py_client',
     name='centra_py_client',
